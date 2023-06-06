cf09ee82b926466426854c5f1a44de23cd7d5785
Dennis D <dennis.drueck@tempus.com>
2023-06-05 23:02:45 -0500

    prompt

---
+++ prompts/symptom-assessment.ts
@@ -0,0 +1,13 @@
+export const prompt = `You are a professional and courteous consultant who helps patients find the correct type of doctor by asking them
+their symptoms. You should gather specific symptoms from the patient, enough to recommend them your top 3 suggestions
+specific so you can make a better reccomendation. Be sure to keep the conversation on topic. Do not provide any
+for which type of doctor to see. If the patient gives you symptoms that are too general, you can ask them to be more
+recommendations for how to treat their symptoms, other than to see a professional. If the user asks questions related
+to their symptoms and how to treat them themselves, do not answer because it is better that the doctor answer their
+questions.`;
+
+export const samplePhrases = [
+  "My stomach hurts.",
+  "I am coughing a lot.",
+  "I feel tired often.",
+];
--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -1,14 +1,13 @@
 export const Welcome: React.FC = () => (
   <div className="bg-white border-gray-100 border-2 rounded-lg px-8 py-5 mr-20 w-full">
-    <h1 className="text-2xl font-bold mb-2">🤖 Hello, I am Daneel</h1>
+    <h1 className="text-2xl font-bold mb-2">🤖 Hello, I am Maria.</h1>
     <p>
-      You can use me to create your own chat bot app. In this demo I am an
-      expert in movie reviews, so feel free to ask me about movies! Then, create
-      your own bot in under five minutes.
+      I can help you find the right type of doctor to help you with your
+      symptoms. Please tell me about your symptoms.
     </p>
     <p>
-      <a className="underline" href="https://github.com/ascorbic/daneel#readme">
-        Build your own
+      <a className="underline" href="#">
+        Disclaimer
       </a>
     </p>
   </div>
